50c792b07ca1b9609f27d3c163be0c92762d7934
mendola <mendola.a@husky.neu.edu>
2020-05-31 16:49:04 -0400

    Initial Commit

---
+++ resources/board/board.mk
@@ -0,0 +1,9 @@
+# List of all the board related files.
+BOARDSRC = $(PROJ_ROOT)/resources/board/board.c
+
+# Required include directories
+BOARDINC = $(PROJ_ROOT)/resources/board
+
+# Shared variables
+ALLCSRC += $(BOARDSRC)
+ALLINC  += $(BOARDINC)
