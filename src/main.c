c47c6de6660e86a2bfc4e5b1e1e891a85f559641
mendola <mendola.a@husky.neu.edu>
2020-08-17 20:11:33 -0400

    AUDIO INPUTS WORK

--- src/main.c
+++ src/main.c
@@ -25,7 +25,7 @@
 #define ADC_GRP2_NUM_CHANNELS   8
 #define ADC_GRP2_BUF_DEPTH      16
 
-#define ADC_GROUP_NUM_CHANNELS   6
+#define ADC_GROUP_NUM_CHANNELS   5
 #define ADC_GROUP_BUF_DEPTH      1
 
 // static adcsample_t samples1[ADC_GRP1_NUM_CHANNELS * ADC_GRP1_BUF_DEPTH];
@@ -39,26 +39,24 @@ static adcsample_t g_adc_samples_buf[ADC_GROUP_NUM_CHANNELS * ADC_GROUP_BUF_DEPT
  * Channels:    IN0 - IN5
  */
 static const ADCConversionGroup g_adc_grp_config = {
-  TRUE,                                 /* circular */
+  FALSE,                                 /* circular */
   ADC_GROUP_NUM_CHANNELS,              /* num_channels */
   NULL,                                /* end_cb   */
   NULL,                                /* error_cb */
   0, 0,                                /* CR1, CR2  */
   0,                                   /* SMPR1 (ch 10-17) */
-  ADC_SMPR2_SMP_AN0(ADC_SAMPLE_41P5)   /* SMPR2*/
-    | ADC_SMPR2_SMP_AN1(ADC_SAMPLE_41P5)
-    | ADC_SMPR2_SMP_AN2(ADC_SAMPLE_41P5)
-    | ADC_SMPR2_SMP_AN3(ADC_SAMPLE_41P5)
+  ADC_SMPR2_SMP_AN3(ADC_SAMPLE_41P5)   /* SMPR2*/
     | ADC_SMPR2_SMP_AN4(ADC_SAMPLE_41P5)
-    | ADC_SMPR2_SMP_AN5(ADC_SAMPLE_41P5),
+    | ADC_SMPR2_SMP_AN5(ADC_SAMPLE_41P5)
+    | ADC_SMPR2_SMP_AN6(ADC_SAMPLE_41P5)
+    | ADC_SMPR2_SMP_AN7(ADC_SAMPLE_41P5),
   ADC_SQR1_NUM_CH(ADC_GROUP_NUM_CHANNELS), /* sqr1 sequence steps 13-16, seq len */
   0,                                       /* sqr2 sequence steps 7-12 */
-  ADC_SQR3_SQ1_N(ADC_CHANNEL_IN0)         /* sqr3 sequence steps 1-6  */
-    | ADC_SQR3_SQ2_N(ADC_CHANNEL_IN1)
-    | ADC_SQR3_SQ3_N(ADC_CHANNEL_IN2)
-    | ADC_SQR3_SQ4_N(ADC_CHANNEL_IN3)
-    | ADC_SQR3_SQ5_N(ADC_CHANNEL_IN4)
-    | ADC_SQR3_SQ6_N(ADC_CHANNEL_IN5)
+  ADC_SQR3_SQ1_N(ADC_CHANNEL_IN3)         /* sqr3 sequence steps 1-6  */
+    | ADC_SQR3_SQ2_N(ADC_CHANNEL_IN4)
+    | ADC_SQR3_SQ3_N(ADC_CHANNEL_IN5)
+    | ADC_SQR3_SQ4_N(ADC_CHANNEL_IN6)
+    | ADC_SQR3_SQ5_N(ADC_CHANNEL_IN7)
 };
 
 /*
@@ -93,6 +91,18 @@ int main(void) {
   halInit();
   chSysInit();
   InitDac();
+  palSetPadMode(GPIOA, 3, PAL_MODE_INPUT_ANALOG);
+  palSetPadMode(GPIOA, 4, PAL_MODE_INPUT_ANALOG);
+  palSetPadMode(GPIOA, 5, PAL_MODE_INPUT_ANALOG);
+  palSetPadMode(GPIOA, 6, PAL_MODE_INPUT_ANALOG);
+  palSetPadMode(GPIOA, 7, PAL_MODE_INPUT_ANALOG);
+
+  palSetPadMode(GPIOB, 3, PAL_MODE_OUTPUT_PUSHPULL);
+  palSetPadMode(GPIOB, 6, PAL_MODE_OUTPUT_PUSHPULL);
+  palSetPadMode(GPIOB, 9, PAL_MODE_OUTPUT_PUSHPULL);
+
+  adcStart(&ADCD1, NULL);
+
   /*
    * Setting up analog inputs used by the demo.
    */
@@ -107,7 +117,6 @@ int main(void) {
   /*
    * Activates the ADC1 driver and the temperature sensor.
    */
-  //adcStart(&ADCD1, NULL);
 
   /*
    * Linear conversion.
@@ -126,23 +135,33 @@ int main(void) {
   int16_t ch1_val = 0;
   int16_t ch2_val = 0;
   chThdSleepMilliseconds(2000);
-  double x = 0.0;
-  #define PI (3.14159265)
-  #define PI_OVER_2  (PI / 2.0)
-  #define OFFSET  (DAC_OUT_MAX / 2)
-  double dx = PI / 50;
+  palSetPad(GPIOB, 3);
+  palSetPad(GPIOB, 6);
+  palSetPad(GPIOB, 9);
 
-  double amplitude = 0;
-  double d_amplitude = 0.001;
   while (true) {
-    amplitude += d_amplitude;
-    if (amplitude > 1.0) {
-      amplitude = 0;
-    }
-    x += dx;
-    ch1_val = (int16_t)(sin(x) * OFFSET * amplitude) + OFFSET;
-    ch2_val = (int16_t)(sin(x + PI_OVER_2) * OFFSET) + OFFSET;
-    TransmitSamples((uint16_t)ch1_val, (uint16_t)ch2_val);
-  }
+    adcConvert(&ADCD1, &g_adc_grp_config, g_adc_samples_buf, ADC_GROUP_BUF_DEPTH);
+    uint16_t left = g_adc_samples_buf[3];
+    uint16_t right = g_adc_samples_buf[4];
+    TransmitSamples(left, right);
+}
+  // double x = 0.0;
+  // #define PI (3.14159265)
+  // #define PI_OVER_2  (PI / 2.0)
+  // #define OFFSET  (DAC_OUT_MAX / 2)
+  // double dx = PI / 50;
+
+  // double amplitude = 0;
+  // double d_amplitude = 0.001;
+  // while (true) {
+  //   amplitude += d_amplitude;
+  //   if (amplitude > 1.0) {
+  //     amplitude = 0;
+  //   }
+  //   x += dx;
+  //   ch1_val = (int16_t)(sin(x) * OFFSET * amplitude) + OFFSET;
+  //   ch2_val = (int16_t)(sin(x + PI_OVER_2) * OFFSET) + OFFSET;
+  //   TransmitSamples((uint16_t)ch1_val, (uint16_t)ch2_val);
+  // }
   return 0;
 }
